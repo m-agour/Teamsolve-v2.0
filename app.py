20f28ed283fb4c4e1493cc1700123f548dc98a63
m-agour <mo.aggour@gmail.com>
2021-09-27 20:02:26 +0200

    socket connection to window.location.origin

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
     print(port)
-    socketio.run(app, host='0.0.0.0', debug=True, port=port)
+    socketio.run(app, host='0.0.0.0', debug=True, port=5000)
     # socketio.run(app)
 
