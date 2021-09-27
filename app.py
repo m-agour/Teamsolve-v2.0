62fed3cc25e9f06a9e0f789019376e6d00052d56
m-agour <mo.aggour@gmail.com>
2021-09-27 18:37:14 +0200

    edited ip

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
+    print(port)
     socketio.run(app, host='0.0.0.0', debug=True, port=port)
     # socketio.run(app)
 
